72d70278e0d5e4aefd7d1f1bcad118e00ce76be1
Paul Golde <94673000+paulpaule21@users.noreply.github.com>
2025-05-20 11:19:14 +0200

    qfexec working

--- lua/paulgolde/plugins/commands.lua
+++ lua/paulgolde/plugins/commands.lua
@@ -1,31 +1,39 @@
-local qfexec = require("paulgolde.qfexec")
+-- ~/.config/nvim/lua/paulgolde/plugins/commands.lua
+return {
+  dir = vim.fn.stdpath("config") .. "/lua/paulgolde/plugins", -- <-- valid local path
+  name = "qfexec-commands",
+  event = "VeryLazy",
+  config = function()
+    local qfexec = require("paulgolde.qfexec") -- ✅ or "qfexec" if in runtime path
 
-local function grep(p)
-  local grepprg = vim.api.nvim_get_option_value("grepprg", {})
-  qfexec.exec({ grepprg, p.args }, false)
-end
+    local function grep(p)
+      local grepprg = vim.api.nvim_get_option_value("grepprg", {})
+      qfexec.exec({ grepprg, p.args }, false)
+    end
 
-local function make(p)
-  local makeprg = vim.api.nvim_get_option_value("makeprg", {})
-  qfexec.exec({ makeprg, p.args }, true)
-end
+    local function make(p)
+      local makeprg = vim.api.nvim_get_option_value("makeprg", {})
+      qfexec.exec({ makeprg, p.args }, true)
+    end
 
-local function exec(p)
-  qfexec.exec({ p.args }, true)
-end
+    local function exec(p)
+      qfexec.exec({ p.args }, true)
+    end
 
-vim.api.nvim_create_user_command("Grep", grep, {
-  nargs = "+",
-  complete = "file",
-  desc = "Run grepprg and send to quickfix",
-})
+    vim.api.nvim_create_user_command("Grep", grep, {
+      nargs = "+",
+      complete = "file",
+      desc = "Run grepprg and send to quickfix",
+    })
 
-vim.api.nvim_create_user_command("Make", make, {
-  nargs = "*",
-  desc = "Run makeprg and send to quickfix",
-})
+    vim.api.nvim_create_user_command("Make", make, {
+      nargs = "*",
+      desc = "Run makeprg and send to quickfix",
+    })
 
-vim.api.nvim_create_user_command("Term", exec, {
-  nargs = "+",
-  desc = "Run shell command and send to quickfix",
-})
+    vim.api.nvim_create_user_command("Term", exec, {
+      nargs = "+",
+      desc = "Run shell command and send to quickfix",
+    })
+  end,
+}
