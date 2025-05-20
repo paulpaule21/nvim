50b25af75488b9e819300a4071e089c66833eaba
Paul Golde <94673000+paulpaule21@users.noreply.github.com>
2025-05-20 11:23:48 +0200

    eslint autoformat

--- lua/paulgolde/plugins/lsp/lspconfig.lua
+++ lua/paulgolde/plugins/lsp/lspconfig.lua
@@ -1,74 +1,123 @@
 return {
-  "williamboman/mason.nvim",
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "neovim/nvim-lspconfig", -- ✅ needed to call lspconfig[server].setup()
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/neodev.nvim", opts = {} },
   },
   config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
     local lspconfig = require("lspconfig")
+    local mason_lspconfig = require("mason-lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local keymap = vim.keymap
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
-
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-      },
-    })
-
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "stylua",
-        "eslint_d",
-      },
-    })
-
-    -- Optional: LSP capabilities from nvim-cmp
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-    if has_cmp then
-      capabilities = cmp_nvim_lsp.default_capabilities()
+    -- Diagnostic signs
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    -- Set up each LSP server
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = capabilities,
-        })
-      end,
+    -- on_attach function
+    local function on_attach(client, bufnr)
+      local opts = { buffer = bufnr, silent = true }
 
-      -- Optional: custom settings for lua_ls
-      ["lua_ls"] = function()
-        lspconfig.lua_ls.setup({
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" },
-              },
-              workspace = {
-                checkThirdParty = false,
-              },
+      opts.desc = "Show LSP references"
+      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+
+      opts.desc = "Go to declaration"
+      keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+      opts.desc = "Show LSP definitions"
+      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+
+      opts.desc = "Show LSP implementations"
+      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+
+      opts.desc = "Show LSP type definitions"
+      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
+      opts.desc = "See available code actions"
+      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+      opts.desc = "Smart rename"
+      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+      opts.desc = "Show buffer diagnostics"
+      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+
+      opts.desc = "Show line diagnostics"
+      keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+      opts.desc = "Go to previous diagnostic"
+      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+
+      opts.desc = "Go to next diagnostic"
+      keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+      opts.desc = "Show documentation for what is under cursor"
+      keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+      opts.desc = "Restart LSP"
+      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+
+      if client.name == "eslint" then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          callback = function()
+            vim.cmd("EslintFixAll")
+          end,
+        })
+      elseif client.supports_method("textDocument/formatting") then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end,
+        })
+      end
+    end
+
+    -- Setup mason-lspconfig
+    mason_lspconfig.setup()
+
+    local servers = mason_lspconfig.get_installed_servers()
+
+    for _, server_name in ipairs(servers) do
+      local opts = {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      }
+
+      if server_name == "lua_ls" then
+        opts.settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            completion = {
+              callSnippet = "Replace",
             },
           },
-        })
-      end,
-    })
+        }
+      elseif server_name == "eslint" then
+        opts.settings = {
+          format = { enable = true },
+        }
+        opts.root_dir = lspconfig.util.root_pattern(
+          ".eslintrc",
+          ".eslintrc.js",
+          ".eslintrc.cjs",
+          ".eslintrc.json",
+          "eslint.config.js",
+          "eslint.config.mjs",
+          "package.json"
+        )
+      end
+
+      lspconfig[server_name].setup(opts)
+    end
   end,
 }
