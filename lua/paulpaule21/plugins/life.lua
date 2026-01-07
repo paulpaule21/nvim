f9bcdf41c3b269327e9962bd3a3f29e6d83a4c3b
Paul Golde <94673000+paulpaule21@users.noreply.github.com>
2026-01-07 11:19:07 +0100

    blahblah

--- lua/paulpaule21/plugins/life.lua
+++ lua/paulpaule21/plugins/life.lua
@@ -5,7 +5,41 @@ return {
     "chipsenkbeil/org-roam.nvim",
   },
   config = function()
-    require("life").setup()
+    require("org-roam").setup({
+      directory = "~/.notes/roam",
+      templates = {
+        i = {
+          description = "Idea",
+          template = require("life.templates").quick_capture(),
+          target = "ideas/%[slug].org",
+        },
+        j = {
+          description = "Daily Journal",
+          template = require("life.templates").daily_journal(),
+          target = "journal/%<%Y-%m-%d>.org",
+        },
+        w = {
+          description = "Weekly Review",
+          template = require("life.templates").weekly_review(),
+          target = "reviews/weekly/%<%Y-W%V>.org",
+        },
+        m = {
+          description = "Monthly Review",
+          template = require("life.templates").monthly_review(),
+          target = "reviews/monthly/%<%Y-%m>.org",
+        },
+        y = {
+          description = "Yearly Review",
+          template = require("life.templates").yearly_review(),
+          target = "reviews/yearly/%<%Y>.org",
+        },
+        p = {
+          description = "Person",
+          template = require("life.templates").person_note(),
+          target = "people/%[slug].org",
+        },
+      },
+    })
   end,
 }
 
