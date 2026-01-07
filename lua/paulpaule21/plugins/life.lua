e290d232b22cfefaa3ad4853593d8ab8a20c723d
Paul Golde <94673000+paulpaule21@users.noreply.github.com>
2026-01-07 12:20:59 +0100

    Feature/add life.nvim (#5)
    * removed history
    
    * blahblah
    
    * added vim fugitive

--- lua/paulpaule21/plugins/life.lua
+++ lua/paulpaule21/plugins/life.lua
@@ -5,16 +5,40 @@ return {
     "chipsenkbeil/org-roam.nvim",
   },
   config = function()
-    require("life").setup({
-      leader = "<Leader>l",
-
-      notes_dir = "~/.notes",
-      roam_dir = "~/.notes/roam",
-      journal_dir = "~/.notes/journal",
-      tasks_dir = "~/.notes/tasks",
-      reviews_dir = "~/.notes/reviews",
-
-      enable_agenda_on_start = true,
+    require("org-roam").setup({
+      directory = "~/.notes/roam",
+      templates = {
+        i = {
+          description = "Idea",
+          template = require("life.templates").quick_capture(),
+          target = "ideas/%[slug].org",
+        },
+        j = {
+          description = "Daily Journal",
+          template = require("life.templates").daily_journal(),
+          target = "journal/%<%Y-%m-%d>.org",
+        },
+        w = {
+          description = "Weekly Review",
+          template = require("life.templates").weekly_review(),
+          target = "reviews/weekly/%<%Y-W%V>.org",
+        },
+        m = {
+          description = "Monthly Review",
+          template = require("life.templates").monthly_review(),
+          target = "reviews/monthly/%<%Y-%m>.org",
+        },
+        y = {
+          description = "Yearly Review",
+          template = require("life.templates").yearly_review(),
+          target = "reviews/yearly/%<%Y>.org",
+        },
+        p = {
+          description = "Person",
+          template = require("life.templates").person_note(),
+          target = "people/%[slug].org",
+        },
+      },
     })
   end,
 }
